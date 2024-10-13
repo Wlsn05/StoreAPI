6de00709df6d924190eda79d34138a82e5a1e6da
Wilson Jaimes <tuemail@email.com89468079+Wlsn05@users.noreply.github.com>
2024-10-12 20:30:44 -0500

    modificación de la conexion a la base de datos a mongodb Atlas y configuracion del archivo .env

--- drivers/connect-db.js
+++ drivers/connect-db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
+//const URL = 'mongodb://127.0.0.1:27017/music' para conectar en local
 
-const URL = 'mongodb://127.0.0.1:27017/music'
+const uri = process.env.MONGODB_URI;
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(URL)
+mongoose.connect(uri)
 .then((() => console.log('Connect DB success')))
 .catch((err) => console.log('Connect DB fail' + err))
 
